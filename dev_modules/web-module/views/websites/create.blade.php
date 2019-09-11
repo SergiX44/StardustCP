830fe97f6e41552f09d290e437de286e17a7c04a
Sergio Brighenti <sergio@brighenti.me>
2019-09-11 10:15:05 +0200

    Added pills in add website

--- dev_modules/web-module/views/websites/create.blade.php
+++ dev_modules/web-module/views/websites/create.blade.php
@@ -15,30 +15,57 @@
                     <form method="post" action="{{ route('web.websites.store') }}">
                         @csrf
 
-                        @form_radio([
-                            'name' => 'is_domain',
-                            'label' => 'From',
-                            'items' => [['value' => 'true', 'text' => 'Domain', 'active' => true], ['value' => 'false', 'text' => 'Subdomain']],
-                        ])
+                        <ul class="nav nav-pills nav-justified mb-3">
+                            <li class="nav-item">
+                                <a class="nav-link active" data-toggle="tab" href="#webspace"><i class="fas fa-globe"></i> Webspace</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="#webserver-conf" role="tab"><i class="fas fa-server"></i> Webserver settings</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="#php-conf"><i class="fab fa-php"></i> PHP settings</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="#php-conf"><i class="fas fa-tape"></i> Backups</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" data-toggle="tab" href="#php-conf"><i class="fab fa-git-alt"></i> Git Deploy</a>
+                            </li>
+                        </ul>
+                        <div class="tab-content" id="myTabContent2">
+                            <div class="tab-pane fade show active" id="webspace">
+                                @form_radio([
+                                'name' => 'is_domain',
+                                'label' => 'From',
+                                'items' => [['value' => 'true', 'text' => 'Domain', 'active' => true], ['value' => 'false', 'text' => 'Subdomain']],
+                                ])
 
-                        @form_select([
-                            'label' => 'Parent Domain',
-                            'name' => 'parent_domain',
-                            'options' => [
+                                @form_select([
+                                'label' => 'Parent Domain',
+                                'name' => 'parent_domain',
+                                'options' => [
                                 1 => 'google.com',
                                 12 => 'amazon.com'
-                            ]
-                        ])
+                                ]
+                                ])
 
-                        @form_input(['name' => 'domain_value', 'label' => 'Domain or Subdomain Name'])
+                                @form_input(['name' => 'domain_value', 'label' => 'Domain or Subdomain Name'])
 
-                        @form_select(['name' => 'ipv4', 'label' => 'IPv4', 'options' => []])
-                        
-                        @form_select(['name' => 'ipv6', 'label' => 'IPv6', 'options' => []])
+                                @form_select(['name' => 'ipv4', 'label' => 'IPv4', 'options' => []])
 
-                        @form_switch(['name' => 'php_enabled', 'label' => 'PHP Enabled'])
+                                @form_select(['name' => 'ipv6', 'label' => 'IPv6', 'options' => []])
 
-                        @form_switch(['name' => 'ssl_enabled', 'label' => 'SSL Enabled', 'checked' => true])
+                                @form_switch(['name' => 'php_enabled', 'label' => 'PHP Enabled'])
+
+                                @form_switch(['name' => 'ssl_enabled', 'label' => 'SSL Enabled', 'checked' => true])
+                            </div>
+                            <div class="tab-pane fade" id="webserver-conf">
+                                Sed sed metus vel lacus hendrerit tempus. Sed efficitur velit tortor, ac efficitur est lobortis quis. Nullam lacinia metus erat, sed fermentum justo rutrum ultrices. Proin quis iaculis tellus. Etiam ac vehicula eros, pharetra consectetur dui.
+                            </div>
+                            <div class="tab-pane fade" id="php-conf">
+                                Vestibulum imperdiet odio sed neque ultricies, ut dapibus mi maximus. Proin ligula massa, gravida in lacinia efficitur, hendrerit eget mauris. Pellentesque fermentum, sem interdum molestie finibus, nulla diam varius leo, nec varius lectus elit id dolor.
+                            </div>
+                        </div>
 
                         @form_submit(['cancel' => 'web.websites.index'])
                     </form>
