feb3bc97efe72bc6fccf80c589176a1dbf92bb74
Sergio Brighenti <sergio@brighenti.me>
2019-09-12 23:41:47 +0200

    Working on core installer

--- app/Console/Commands/InstallerCommand.php
+++ app/Console/Commands/InstallerCommand.php
@@ -2,13 +2,13 @@
 
 namespace Core\Console\Commands;
 
-use Core\AuraX\StringBuilder;
 use Core\Environment\OS;
 use Core\Environment\PackageManagers\IPackageManager;
 use Illuminate\Console\Command;
 
 use Illuminate\Support\Facades\Artisan;
 use function Safe\file_get_contents;
+use function Safe\file_put_contents;
 use Symfony\Component\Process\Process;
 
 class InstallerCommand extends Command {
@@ -50,7 +50,7 @@ class InstallerCommand extends Command {
 
 		$env = OS::recognize();
 
-		$result = $this->confirm("Detected {$env->getPrettyName()}. It's correct?");
+		$result = $this->confirm("Detected {$env->getPrettyName()}. It's correct?", true);
 		if (!$result) {
 			$this->error('Cannot detect you system.');
 			exit(1);
@@ -61,7 +61,7 @@ class InstallerCommand extends Command {
 
 		$hostname = trim(file_get_contents('/etc/hostname'));
 
-		$result = $this->confirm("The system hostname is '{$hostname}'. It's correct?");
+		$result = $this->confirm("The system hostname is '{$hostname}'. It's correct?", true);
 		if (!$result) {
 			$this->error('Wrong hostname, aborting.');
 			exit(1);
@@ -70,14 +70,15 @@ class InstallerCommand extends Command {
 		$this->updateSystem($pkg);
 		$this->installDBMS($pkg);
 		$dbmsPassword = $this->configureDBMS();
-		$this->writeDotEnv($hostname, $dbmsPassword);
-
+		$this->configurePanel($hostname, $dbmsPassword);
 
 		Artisan::call('key:generate');
-		Artisan::call('migrate', ['--force']);
+		Artisan::call('migrate', ['--force' => '']);
 		Artisan::call('optimize');
 
-		$this->info('Installation completed!');
+		$this->warn("MySQL root password: {$dbmsPassword}");
+
+		$this->info('Core installation completed!');
 	}
 
 	private function updateSystem(IPackageManager $pkg) {
@@ -101,39 +102,99 @@ class InstallerCommand extends Command {
 		$this->warn('Done.');
 	}
 
+	/**
+	 * @return array
+	 * @throws \Safe\Exceptions\FilesystemException
+	 */
 	private function configureDBMS() {
 		$password = $this->secret('Insert new MariaDB password for "root". (Empty for generate): ');
 		if ($password === null) {
 			$password = str_random(16);
 		}
 
-		$this->info('Configuring MariaDB...');
-		$proc = Process::fromShellCommandline('mysql_secure_installation', null, null,);
+		// secure install and set root password
+		$this->info('Securing installation...');
+		$proc = Process::fromShellCommandline('mysql_secure_installation');
 		$proc->setPty(true);
 		$proc->setInput("\ny\n{$password}\n{$password}\ny\ny\ny\ny\n");
 		$proc->run();
-		$this->warn('Done.');
 
-		// TODO: complete mariasb config, add stardust db
+		$this->info('Configuring services...');
+
+		$myDotConf = '/etc/mysql/mariadb.conf.d/50-server.cnf';
+		if (!file_exists($myDotConf)) {
+			$myDotConf = '/etc/mysql/my.cnf';
+		}
+
+		$content = file_get_contents($myDotConf);
+
+		umask(0644);
+
+		// bind mysql on all interfaces
+		preg_replace('/^(bind-address)/m', '#bind-address', $content, 1);
+
+		file_put_contents($myDotConf, $content);
+
+		// password authentication method in MariaDB to native
+		Process::fromShellCommandline("mysql -uroot -p{$password} -e \"update mysql.user set plugin = 'mysql_native_password' where user='root';\"")->run();
+
+		$this->info('Configure open file limit...');
+
+		//security file
+		$openFileLimit = "\nmysql soft nofile 65535\nmysql hard nofile 65535";
+		file_put_contents('/etc/security/limits.conf', $openFileLimit, FILE_APPEND);
+
+		// systemd config
+		$openFileLimit = "[Service]\nLimitNOFILE=infinity";
+		if (!file_exists('/etc/systemd/system/mysql.service.d/')) {
+			mkdir('/etc/systemd/system/mysql.service.d/', 0755, true);
+		}
+		file_put_contents('/etc/systemd/system/mysql.service.d/limits.conf', $openFileLimit);
+
+		$this->info('Restart service...');
+		Process::fromShellCommandline('systemctl daemon-reload && systemctl restart mysql')->run();
+
+		$this->warn('Done.');
 
 		return $password;
 	}
 
-	private function writeDotEnv($hostname, $dbmsPassword) {
-		$dotEnv = new StringBuilder();
-		$dotEnv->appendLine('APP_ENV=production');
-		$dotEnv->appendLine('APP_KEY=');
-		$dotEnv->appendLine('APP_DEBUG=false');
-		$dotEnv->appendLineFormat('APP_URL=http://{0}:8443', $hostname);
-		$dotEnv->appendLine('LOG_CHANNEL=daily');
-		$dotEnv->appendLine('DB_CONNECTION=mysql');
-		$dotEnv->appendLine('DB_HOST=localhost');
-		$dotEnv->appendLine('DB_PORT=3306');
-		$dotEnv->appendLine('DB_DATABASE=stardustcp');
-		$dotEnv->appendLine('DB_USERNAME=stardust');
-		$dotEnv->appendLine('DB_USERNAME=');
+	/**
+	 * @param $hostname
+	 * @param $dbmsPassword
+	 * @throws \Safe\Exceptions\FilesystemException
+	 */
+	private function configurePanel($hostname, $dbmsPassword) {
 
-		// TODO: complete and write .env config
+		// create database
+		$stardustPassword = str_random(16);
+
+		Process::fromShellCommandline("mysql -uroot -p{$dbmsPassword} -e \"CREATE DATABASE stardust /*\!40100 DEFAULT CHARACTER SET utf8 */;\"")->run();
+		Process::fromShellCommandline("mysql -uroot -p{$dbmsPassword} -e \"CREATE USER stardust@localhost IDENTIFIED BY '{$stardustPassword}';\"")->run();
+		Process::fromShellCommandline("mysql -uroot -p{$dbmsPassword} -e \"GRANT ALL PRIVILEGES ON stardust.* TO 'stardust'@'localhost';\"")->run();
+		Process::fromShellCommandline("mysql -uroot -p{$dbmsPassword} -e \"FLUSH PRIVILEGES;\"")->run();
+
+		$dotEnv = <<< ENV
+APP_ENV=production
+APP_KEY=
+APP_DEBUG=false
+APP_URL=http://{$hostname}:8443
+
+LOG_CHANNEL=daily
+DB_CONNECTION=mysql
+DB_HOST=localhost
+DB_PORT=3306
+DB_DATABASE=stardust
+DB_USERNAME=stardust
+DB_PASSWORD="{$stardustPassword}"
+
+BROADCAST_DRIVER=pusher
+CACHE_DRIVER=file
+QUEUE_CONNECTION=database
+SESSION_DRIVER=file
+SESSION_LIFETIME=120
+ENV;
+		file_put_contents('.env', $dotEnv);
 
 	}
 }
