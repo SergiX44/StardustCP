00ec893500ae2143052b45f119764d7382c0870a
SergiX44 <sergio@brighenti.me>
2019-12-18 15:18:32 +0100

    Updated install script

--- install.sh
+++ install.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 
-if [ "$EUID" -ne 0 ]
+if [[ "$EUID" -ne 0 ]]
   then echo "You must run the installer as root."
   exit 1
 fi
 
-echo "Installing base installer packages..."
+echo "Installing required installer packages..."
 
-apt update >> install.sh.log 2>&1
+apt update > install.sh.log 2>&1
 apt install -y sudo php-cli php-curl php-mysql php-mbstring php-xml php-zip >> install.sh.log 2>&1
 
 echo "Starting installer..."
 
-## TODO: remove, dev purpose
+## TODO: remove, dev purpose ##
 rm .env
 rm -rf .root_db
 rm rr
-##
+php artisan optimize:clear
+###############################
 
 php artisan auto-install "$@"
 
